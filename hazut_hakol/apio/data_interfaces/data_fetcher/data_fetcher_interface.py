62ba4474f2470386ca26b312e4b2dad8f34ff4df
LiavBatson <batsonliav@gmail.com>
2026-08-16 15:44:20 +0300

    added data interfaces

---
+++ hazut_hakol/apio/data_interfaces/data_fetcher/data_fetcher_interface.py
@@ -0,0 +1,37 @@
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import List
+
+from hazut_hakol.core.classes.barak import Sweep
+
+
+class DataFetcherInterface(ABC):
+    @abstractmethod
+    def download_sweep(self, sweep: Sweep, output_dir: Path):
+        ...
+
+    @abstractmethod
+    def download_sweeps(self, sweeps: List[Sweep], output_dir: Path, raise_on_missing: bool = False):
+        ...
+
+    @abstractmethod
+    def download_sweep_asset_by_filename(self,
+                                         sweep: Sweep,
+                                         output_dir: Path,
+                                         asset_filename: str,
+                                         raise_on_missing: bool = False
+    ):
+        ...
+
+    @abstractmethod
+    def download_sweeps_asset_by_filename(self,
+                                          sweeps: List[Sweep],
+                                          output_dir: Path,
+                                          asset_filename: str,
+                                          raise_on_missing: bool = False
+    ):
+        ...
+
+    @abstractmethod
+    def download_grids(self, sweeps: List[Sweep], output_dir: Path, raise_on_missing: bool = False):
+        ...
