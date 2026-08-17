d982405ef288c3165f963a8c4b99c05072a2775b
LiavBatson <batsonliav@gmail.com>
2026-08-17 15:42:59 +0300

    fixed data fetcher paths

--- hazut_hakol/apio/data_interfaces/data_fetcher/data_fetcher.py
+++ hazut_hakol/apio/data_interfaces/data_fetcher/data_fetcher.py
@@ -1,21 +1,23 @@
-import copy
-from loguru import logger
-from retry import retry
 from pathlib import Path
-from typing import List, Dict
+from typing import List
 
+from hazut_hakol.apio.data_interfaces.data_fetcher.data_fetcher_interface import DataFetcherInterface
+from hazut_hakol.apio.data_storage.data_storage_azure import DataStorageAzureNode
+from hazut_hakol.apio.interfaces.azure_storage_interface import AzureStorageInterface
 from hazut_hakol.core.classes.barak.barak_sweep import Sweep
 from hazut_hakol.core.entrex_base_errors import EntrexDataError
 from hazut_hakol.core.utils import Environment
-from hazut_hakol.apio.interfaces.azure_storage_interface import AzureStorageInterface
-from hazut_hakol.apio.data_storage.data_storage_azure import DataStorageAzureNode
-from hazut_hakol.apio.data_interfaces.data_fetcher.data_fetcher_interface import DataFetcherInterface
 
 
 class DataFetcherException(EntrexDataError):
     def __init__(self, message):
         self.message = message
         super().__init__(message)
+
+
+def create_folder_if_needed(output_dir: Path):
+    if not output_dir.exists():
+        output_dir.mkdir(exist_ok=True, parents=True)
 
 
 class DataFetcher(DataFetcherInterface):
@@ -27,6 +29,7 @@ class DataFetcher(DataFetcherInterface):
                                                               container_name=container_name)
 
     def download_sweep(self, sweep: Sweep, output_dir: Path) -> dict:
+        create_folder_if_needed(output_dir=output_dir)
         sweeps_dict = {}
         azure_path = Path(sweep.image_path_in_azure)
         self._azure_storage_interface.download_file(
@@ -37,6 +40,7 @@ class DataFetcher(DataFetcherInterface):
         return sweeps_dict
 
     def download_sweeps(self, sweeps: List[Sweep], output_dir: Path, raise_on_missing: bool = False) -> dict:
+        create_folder_if_needed(output_dir=output_dir)
         sweeps_dict = {}
         for sweep in sweeps:
             azure_path = Path(sweep.image_path_in_azure)
@@ -53,8 +57,9 @@ class DataFetcher(DataFetcherInterface):
                                          asset_filename: str,
                                          raise_on_missing: bool = False
     ):
+        create_folder_if_needed(output_dir=output_dir)
         sweeps_dict = {}
-        file_to_download = Path(sweep.image_path_in_azure).stem / asset_filename
+        file_to_download = Path(Path(sweep.image_path_in_azure).with_name(asset_filename))
         self._azure_storage_interface.download_file(
             blob_name=file_to_download,
             download_path=str(output_dir / file_to_download.name)
@@ -68,9 +73,10 @@ class DataFetcher(DataFetcherInterface):
                                           asset_filename: str,
                                           raise_on_missing: bool = False
     ):
+        create_folder_if_needed(output_dir=output_dir)
         sweeps_dict = {}
         for sweep in sweeps:
-            file_to_download = Path(sweep.image_path_in_azure).stem / asset_filename
+            file_to_download = Path(Path(sweep.image_path_in_azure).with_name(asset_filename))
             self._azure_storage_interface.download_file(
                 blob_name=file_to_download,
                 download_path=str(output_dir / file_to_download.name)
